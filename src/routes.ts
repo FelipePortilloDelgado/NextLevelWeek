f842be09e68d904e9debb42ea4a7a4eb4f308a23
Felipe Portillo Delgado <felipe.portillo@workdoc.com.br>
2021-02-25 11:35:23 -0300

    aula 4

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { SendMailController } from './controllers/SendMailController'
 import { ServeysController } from './controllers/ServeysController'
 import { UserController } from './controllers/UsersController'
 
@@ -7,9 +8,14 @@ const router = Router()
 const userController = new UserController()
 const surveyController = new ServeysController()
 
+const sendMailController = new SendMailController()
+
 router.post('/users', userController.create)
 
 router.post('/surveys', surveyController.create)
 router.get('/surveys', surveyController.show)
+
+router.post('/sendMail', sendMailController.execute)
+router.delete('/surveysusers', sendMailController.deleteAll)
 
 export { router }
